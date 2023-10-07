e2e738618161e458c4d6fee989c107a92c38d655
Andrii Kardash <andrii.kardash@gmail.com>
2023-10-08 00:49:07 +0300

    added conftest file

--- test_epam_page.py
+++ test_epam_page.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
 
-def test_epam_title():
+
+def test_epam_title(driver):
     driver.get('https://www.epam.com/')
     title = 'EPAM | Software Engineering & Product Development Services'
     page_title = driver.title
