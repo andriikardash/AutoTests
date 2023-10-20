25cb13e27f1439c5c983429a8b8c31eb00eda9ba
Andrii Kardash <andrii.kardash@gmail.com>
2023-10-20 22:47:53 +0300

    moved epam tests into folder

--- demowebshop/register_page.py
+++
@@ -1,8 +0,0 @@
-from home import HomePage
-from selenium.webdriver.common.by import By
-
-REGISTRATION_BUTTON = (By.XPATH, '//a[@class="ico-register"]')
-
-class Register(HomePage):
-    def nav_to_register(self):
-        self.find(self.REGISTRATION_BUTTON).click()
